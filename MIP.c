46ca7e0b1ca0afebe036d2e1c339d0199efb8f46
Marcos Vinícius Adão Teixeira <ecclesiedei@gmail.com>
2018-05-07 15:01:17 -0300

    gerando imagens pretas [corrigir]

--- MIP.c
+++ MIP.c
@@ -11,7 +11,12 @@
 #define GetZCoord(s,p) ((p) / (((s)->xsize)*((s)->ysize)))
 
 #define ROUND(x) ((x < 0)?(int)(x-0.5):(int)(x+0.5))
-#define GetVoxelIndex(s,v) ((v.x)+(s)->tby[(v.y)])
+#define GetVoxelIndex(s,v) ((v.x)+(s)->tby[(v.y)]+(s)->tbz[(v.z)])
+
+int VoxelValue(iftImage *img, iftVoxel v)
+{
+    return img->val[GetVoxelIndex(img, v)];
+}
 
 int sign( int x ){
     if(x >= 0)
@@ -72,45 +77,59 @@ int DDA(iftImage *img, iftVoxel p1, iftVoxel pn)
 {
     int n, k;
     //iftVoxel p;
-    float px, py;
+    iftVoxel p;
     float J=0, max=0;
-    int Dx,Dy;
-    float dx=0,dy=0;
+    int Dx,Dy,Dz;
+    float dx=0,dy=0,dz=0;
     //iftCreateMatrix* J;
 
 
-    if (p1.x == pn.x && p1.y == pn.y)
+    if (p1.x == pn.x && p1.y == pn.y && p1.z == pn.z)
         n=1;
     else{
         Dx=pn.x - p1.x;
         Dy=pn.y - p1.y;
+        Dz=pn.z - p1.z;
 
-        if( abs(Dx) >= abs(Dy) ){
+        if( abs(Dx) >= abs(Dy) && abs(Dx) >= abs(Dz) ){
             n = abs(Dx)+1;
             dx = sign(Dx);
             dy = (dx * Dy)/Dx;
+            dz = (dx * Dz)/Dx;
         }
-        else{
+        else if ( abs(Dy) >= abs(Dx) && abs(Dy) >= abs(Dz)){
             n = abs(Dy)+1;
             dy = sign(Dy);
             dx = (dy * Dx)/Dy;
+            dz = (dy * Dz)/Dy;
+        }
+        else{
+            n = abs(Dz)+1;
+            dz = sign(Dz);
+            dx = (dz * Dx)/Dz;
+            dy = (dz * Dy)/Dz;
         }
     }
 
-    px = p1.x;
-    py = p1.y;
+    p.x = p1.x;
+    p.y = p1.y;
+    p.z = p1.z;
 
 
     for (k = 1; k < n; k++)
     {
+        //printf("debug\n");
         //J+=  (float)LinearInterpolationValue(img, px, py);
-        J+=  iftImgVal2D(img, (int)px, (int)py);
+        //J+=  iftImgVal2D(img, (int)px, (int)py);
+        J+= VoxelValue(img,p);
+        
         if (J>max)
           max=J;
         //J+=  (float)LinearInterpolationValue(img, px, py);
 
-        px = px + dx;
-        py = py + dy;
+        p.x = p.x + dx;
+        p.y = p.y + dy;
+        p.z = p.z + dz;
     }
 
     return (int)max;
@@ -120,7 +139,8 @@ int DDA(iftImage *img, iftVoxel p1, iftVoxel pn)
 int isValidPoint(iftImage *img, iftVoxel u)
 {
     if ((u.x >= 0) && (u.x < img->xsize) &&
-        (u.y >= 0) && (u.y < img->ysize)){
+        (u.y >= 0) && (u.y < img->ysize) &&
+        (u.z >= 0) && (u.z < img->zsize)){
         return 1;
     }
     else{
@@ -129,21 +149,23 @@ int isValidPoint(iftImage *img, iftVoxel u)
 }
 
 
-int ComputeIntersection(iftMatrix *Tpo, iftImage *img, iftMatrix *Tn, iftVolumeFaces *vf, int *p1, int *pn)
+int ComputeIntersection(iftMatrix *Tpo, iftImage *img, iftMatrix *Tn, iftVolumeFaces *vf, iftVoxel *p1, iftVoxel *pn)
 {
 
     float lambda[6] = { -1};
-    int i, p_aux;
+    int i;
+    p1->x=pn->x=p1->y=pn->y,p1->z=pn->z=-1;
     iftMatrix *Nj = iftCreateMatrix(1, 3);
     iftMatrix *DiffCandP0 = iftCreateMatrix(1, 3);
     float NdotNj = 0, DiffShiftDotNj = 0;
-    iftVoxel v;
-    *p1 = *pn = -1;
+    iftVoxel v; 
 
     for (i = 0; i < 6; i++) {
       iftMatrixElem(Nj, 0, 0) = vf[i].orthogonal->val[AXIS_X];
       iftMatrixElem(Nj, 0, 1) = vf[i].orthogonal->val[AXIS_Y];
       iftMatrixElem(Nj, 0, 2) = vf[i].orthogonal->val[AXIS_Z];
+      
+
 
       NdotNj = MatrixInnerProduct(Tn,Nj);
 
@@ -154,6 +176,11 @@ int ComputeIntersection(iftMatrix *Tpo, iftImage *img, iftMatrix *Tn, iftVolumeF
       DiffShiftDotNj = MatrixInnerProduct(DiffCandP0,Nj);
 
       lambda[i]=(float) DiffShiftDotNj/NdotNj;
+      //iftPrintMatrix(DiffCandP0);
+      //iftPrintMatrix(Nj);
+      //printf("%f, %f\n", DiffShiftDotNj, NdotNj );
+      
+      //printf("%f\n", lambda[i]);
 
       v.x = ROUND(Tpo->val[AXIS_X] + lambda[i] * Tn->val[AXIS_X]);
       v.y = ROUND(Tpo->val[AXIS_Y] + lambda[i] * Tn->val[AXIS_Y]);
@@ -162,31 +189,28 @@ int ComputeIntersection(iftMatrix *Tpo, iftImage *img, iftMatrix *Tn, iftVolumeF
       if (isValidPoint(img, v))
       {
 
-          if (*p1 == -1){
-              *p1 = GetVoxelIndex(img, v);
+          if (p1->x == -1){
+              p1->x = v.x;
+              p1->y = v.y;
+              p1->z = v.z;
           }
-          else if (*p1 != -1){
-              if (*p1 != GetVoxelIndex(img, v)){
-                  *pn = GetVoxelIndex(img, v);
-                  printf("ponto valido2\n");
-              }
+          else {
+              pn->x = v.x;
+              pn->y = v.y;
+              pn->z = v.z;
           }
       }
 
     }
+  
+    
 
-    // swap is p1 is greater than pn
-    if (*p1 > *pn)
-    {
-        p_aux = *p1;
-        *p1 = *pn;
-        *pn = p_aux;
-    }
-
+  
     iftDestroyMatrix(&Nj);
     iftDestroyMatrix(&DiffCandP0);
-    if ((*p1 != -1) && (*pn != -1))
+    if ((p1->x != -1) && (pn->z != -1)){
         return 1;
+      }
     else
         return 0;
 }
@@ -226,7 +250,7 @@ int LinearInterpolationValue(iftImage *img, float x, float y, float z)
     P56 = (float)iftImgVal2D(img,u[5].x,u[5].y) * (x - u[4].x) + (float)iftImgVal2D(img,u[4].x,u[4].y) * (u[5].x - x);
     P78 = (float)iftImgVal2D(img,u[7].x,u[7].y) * (x - u[6].x) + (float)iftImgVal2D(img,u[6].x,u[6].y) * (u[7].x - x);
     aux1 = P34 *  (y - u[0].y) + P12 * (u[2].y - y);
-    aux2 = P56] * (y - u[0].y) + P78 * (u[2].y - y);
+    aux2 = P56 * (y - u[0].y) + P78 * (u[2].y - y);
     Pi  = (int)aux2 * (z - u[0].z) + aux1 * (u[4].z - z);
 
     return Pi;
@@ -328,6 +352,17 @@ iftMatrix *voxelToMatrix(iftVoxel v)
     return voxMat;
 }
 
+iftMatrix *imagePixelToMatrix(iftImage *img, int p)
+{
+    iftMatrix *pixMat = iftCreateMatrix(1, 4);
+    iftMatrixElem(pixMat, 0, 0) = p % img->xsize;
+    iftMatrixElem(pixMat, 0, 1) = p / img->xsize;
+    iftMatrixElem(pixMat, 0, 2) = 0;
+    iftMatrixElem(pixMat, 0, 3) = 1;
+
+    return pixMat;
+}
+
 
 
 iftImage *MaximumIntensityProjection(iftImage *img, float xtheta, float ytheta)
@@ -336,10 +371,8 @@ iftImage *MaximumIntensityProjection(iftImage *img, float xtheta, float ytheta)
     double maxIntensity;
     int p=0;
     int Nu, Nv;
-    int p1, pn;
-    //float* lineVals;
-    iftVoxel p0, v1, vn;
-    //
+    iftVoxel p1, pn;
+    
     iftVolumeFaces* volumeFaces;
     iftMatrix *Mtemp, *T;
     iftMatrix *Norigin, *Tnorigin;
@@ -354,7 +387,7 @@ iftImage *MaximumIntensityProjection(iftImage *img, float xtheta, float ytheta)
     Norigin =  iftCreateMatrix(1, 4);
     iftMatrixElem(Norigin, 0, 0) = 0;
     iftMatrixElem(Norigin, 0, 1) = 0;
-    iftMatrixElem(Norigin, 0, 2) = 0;
+    iftMatrixElem(Norigin, 0, 2) = 1;
     iftMatrixElem(Norigin, 0, 3) = 1;
     //iftPrintMatrix(Norigin);
 
@@ -364,19 +397,19 @@ iftImage *MaximumIntensityProjection(iftImage *img, float xtheta, float ytheta)
 
     for (p = 0; p < output->n; p++)
     {
-        p1 = pn = -1;
-        p0 = GetVoxelCoord(output, p);
-        Mtemp = voxelToMatrix(p0);
+        Mtemp = imagePixelToMatrix(output,p);
+        
         Tpo = iftMultMatrices(T, Mtemp);
 
         if (ComputeIntersection(Tpo, img, Tnorigin, volumeFaces, &p1, &pn))
         {
-            printf("passou\n");
-            v1 = GetVoxelCoord(img, p1);
-            vn = GetVoxelCoord(img, pn);
+            
+            //v1 = GetVoxelCoord(img, p1);
+            //vn = GetVoxelCoord(img, pn);
 
             //maxIntensity = IntensityProfile(img, v1, vn);
-            maxIntensity  = DDA(img,v1,vn);
+            maxIntensity  = DDA(img,p1,pn);
+            //printf("%lf\n", maxIntensity);
 
             output->val[p] = maxIntensity;
         }
@@ -412,7 +445,7 @@ int main(int argc, char *argv[])
     //tz = atof(argv[5]);
     char *imgFileName = iftCopyString(argv[1]);
     iftImage *img = iftReadImageByExt(imgFileName);
-    iftWriteImageByExt(img, "test.png");
+  
     iftImage *output = NULL;
 
     output = MaximumIntensityProjection(img, tx, ty);
